6be690d43343327720508badd391f891d726e69d
Ana Clara Fagundes <79975018+fagundesana017@users.noreply.github.com>
2022-01-24 17:54:04 -0300

    preparando o projeto marvel(fontes, imagem, home page, colors)

---
+++ lib/themes/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const primaryButton = Color(0xFFE21938);
+const textSecondary = Color(0xFF69686D);
+const colorTitles = Color(0xFFFFFFFF);
+const blackGroundColor = Color(0xff161618);
+const secondaryBlack = Color(0xff232227);
+const starColor = Color(0xFFF7A10F);
