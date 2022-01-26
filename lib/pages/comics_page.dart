a553016bd4cba1df2510044e4a444fe0404e8fdc
ItaloCovas <italocovas@gmail.com>
2022-01-26 14:02:20 -0300

    Corrigindo erros de constraint e scroll

--- lib/pages/comics_page.dart
+++ lib/pages/comics_page.dart
@@ -22,7 +22,6 @@ class _ComicsPageState extends State<ComicsPage> {
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
     var height = MediaQuery.of(context).size.height;
     return Scaffold(
       resizeToAvoidBottomInset: true,
@@ -45,111 +44,122 @@ class _ComicsPageState extends State<ComicsPage> {
             child: const Icon(Icons.arrow_back_ios_new)),
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              child: Observer(builder: (_) {
-                if (comicsStore.comicsModel != null) {
-                  return Padding(
-                    padding: const EdgeInsets.only(top: 20, bottom: 20),
-                    child: CarouselSlider.builder(
-                      options: CarouselOptions(
-                        height: 250,
-                        aspectRatio: 20 / 15,
-                        viewportFraction: 0.5,
-                        initialPage: 0,
-                        enableInfiniteScroll: true,
-                        reverse: false,
-                        autoPlay: true,
-                        autoPlayInterval: const Duration(seconds: 3),
-                        autoPlayAnimationDuration:
-                            const Duration(milliseconds: 800),
-                        autoPlayCurve: Curves.fastOutSlowIn,
-                        enlargeCenterPage: true,
-                        scrollDirection: Axis.horizontal,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                child: Observer(builder: (_) {
+                  if (comicsStore.comicsModel != null) {
+                    return Padding(
+                      padding: const EdgeInsets.only(top: 20, bottom: 20),
+                      child: CarouselSlider.builder(
+                        options: CarouselOptions(
+                          height: 250,
+                          aspectRatio: 20 / 15,
+                          viewportFraction: 0.5,
+                          initialPage: 0,
+                          enableInfiniteScroll: true,
+                          reverse: false,
+                          autoPlay: true,
+                          autoPlayInterval: const Duration(seconds: 3),
+                          autoPlayAnimationDuration:
+                              const Duration(milliseconds: 800),
+                          autoPlayCurve: Curves.fastOutSlowIn,
+                          enlargeCenterPage: true,
+                          scrollDirection: Axis.horizontal,
+                        ),
+                        itemCount: comicsStore.comicsModel?.length,
+                        itemBuilder: (BuildContext context, int index,
+                            int pageViewIndex) {
+                          var comics = comicsStore.comicsModel![index];
+                          return Container(
+                            child: Image.network(comics.thumbnailPath +
+                                "." +
+                                comics.thumbnailExtension),
+                          );
+                        },
                       ),
-                      itemCount: comicsStore.comicsModel?.length,
-                      itemBuilder:
-                          (BuildContext context, int index, int pageViewIndex) {
-                        var comics = comicsStore.comicsModel![index];
-                        return Container(
-                          child: Image.network(comics.thumbnailPath +
-                              "." +
-                              comics.thumbnailExtension),
-                        );
-                      },
-                    ),
+                    );
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                }),
+              ),
+              Observer(builder: (_) {
+                if (comicsStore.comicsModel != null) {
+                  return Flexible(
+                    fit: FlexFit.loose,
+                    child: GridView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 8,
+                          mainAxisSpacing: 8,
+                          childAspectRatio: 0.75,
+                        ),
+                        itemCount: comicsStore.comicsModel?.length,
+                        itemBuilder: (ctx, index) {
+                          var comics = comicsStore.comicsModel![index];
+                          return GestureDetector(
+                            onTap: () {
+                              print('click');
+                            },
+                            child: Container(
+                                margin:
+                                    const EdgeInsets.only(left: 5, right: 5),
+                                decoration: BoxDecoration(
+                                    color: secondaryBlack,
+                                    borderRadius: BorderRadius.circular(10)),
+                                child: Column(
+                                  children: [
+                                    const SizedBox(
+                                      height: 8,
+                                    ),
+                                    Center(
+                                      child: Text(
+                                        comics.title,
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 8,
+                                    ),
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        border: Border.all(
+                                            color: Colors.grey.shade400,
+                                            width: 2),
+                                      ),
+                                      child: Image.network(
+                                        comics.thumbnailPath +
+                                            "." +
+                                            comics.thumbnailExtension,
+                                        width: 140,
+                                        height: 180,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ],
+                                )),
+                          );
+                        }),
                   );
                 } else {
                   return const Center(
                     child: CircularProgressIndicator(),
                   );
                 }
-              }),
-            ),
-            Observer(builder: (_) {
-              if (comicsStore.comicsModel != null) {
-                return Flexible(
-                  child: GridView.builder(
-                      shrinkWrap: true,
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        crossAxisSpacing: 8.0,
-                        mainAxisSpacing: 8.0,
-                      ),
-                      itemCount: comicsStore.comicsModel?.length,
-                      itemBuilder: (ctx, index) {
-                        var comics = comicsStore.comicsModel![index];
-                        return GestureDetector(
-                          child: Container(
-                              margin: const EdgeInsets.only(left: 5, right: 5),
-                              width: 90,
-                              height: 90,
-                              decoration: BoxDecoration(
-                                  color: secondaryBlack,
-                                  borderRadius: BorderRadius.circular(10)),
-                              child: Column(
-                                children: [
-                                  const SizedBox(
-                                    height: 8,
-                                  ),
-                                  Center(
-                                    child: Text(
-                                      comics.title,
-                                      style:
-                                          const TextStyle(color: Colors.white),
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                    height: 8,
-                                  ),
-                                  Container(
-                                    decoration: BoxDecoration(
-                                        border: Border.all(
-                                            color: Colors.grey.shade400,
-                                            width: 2)),
-                                    child: Image.network(
-                                      comics.thumbnailPath +
-                                          "." +
-                                          comics.thumbnailExtension,
-                                      width: 180,
-                                      height: 180,
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                ],
-                              )),
-                        );
-                      }),
-                );
-              } else {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-            })
-          ],
+              })
+            ],
+          ),
         ),
       ),
     );
