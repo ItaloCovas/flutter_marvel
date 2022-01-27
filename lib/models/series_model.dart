e50aa5c990e2cb15854b81281bdbe0235f9292b3
ItaloCovas <italocovas@gmail.com>
2022-01-27 10:54:51 -0300

    Adicionando model de Series

--- lib/models/series_model.dart
+++ lib/models/series_model.dart
@@ -1,98 +1,8 @@
 class SeriesModel {
-  int code;
-  String status;
-  String copyright;
-  String attributionText;
-  String attributionHTML;
-  String etag;
-  Data data;
-
   SeriesModel({
-    required this.code,
-    required this.status,
-    required this.copyright,
-    required this.attributionText,
-    required this.attributionHTML,
-    required this.etag,
-    required this.data,
-  });
-
-  factory SeriesModel.from(SeriesModel serie) {
-    return SeriesModel(
-      code: serie.code,
-      status: serie.status,
-      copyright: serie.copyright,
-      attributionText: serie.attributionText,
-      attributionHTML: serie.attributionHTML,
-      etag: serie.etag,
-      data: serie.data,
-    );
-  }
-
-  factory SeriesModel.fromJson(Map<String, dynamic> response) {
-    return SeriesModel(
-      code: response['code'],
-      status: response['status'],
-      copyright: response['copyright'],
-      attributionText: response['attributionText'],
-      attributionHTML: response['attributionHTML'],
-      etag: response['etag'],
-      data: response['data'],
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    final _data = <String, dynamic>{};
-    _data['code'] = code;
-    _data['status'] = status;
-    _data['copyright'] = copyright;
-    _data['attributionText'] = attributionText;
-    _data['attributionHTML'] = attributionHTML;
-    _data['etag'] = etag;
-    _data['data'] = data.toJson();
-    return _data;
-  }
-}
-
-class Data {
-  Data({
-    required this.offset,
-    required this.limit,
-    required this.total,
-    required this.count,
-    required this.results,
-  });
-  late final int offset;
-  late final int limit;
-  late final int total;
-  late final int count;
-  late final List<Results> results;
-
-  Data.fromJson(Map<String, dynamic> json) {
-    offset = json['offset'];
-    limit = json['limit'];
-    total = json['total'];
-    count = json['count'];
-    results =
-        List.from(json['results']).map((e) => Results.fromJson(e)).toList();
-  }
-
-  Map<String, dynamic> toJson() {
-    final _data = <String, dynamic>{};
-    _data['offset'] = offset;
-    _data['limit'] = limit;
-    _data['total'] = total;
-    _data['count'] = count;
-    _data['results'] = results.map((e) => e.toJson()).toList();
-    return _data;
-  }
-}
-
-class Results {
-  Results({
     required this.id,
     required this.title,
-    this.description,
+    required this.description,
     required this.resourceURI,
     required this.urls,
     required this.startYear,
@@ -106,12 +16,10 @@ class Results {
     required this.stories,
     required this.comics,
     required this.events,
-    this.next,
-    this.previous,
   });
   late final int id;
   late final String title;
-  late final String? description;
+  late final String description;
   late final String resourceURI;
   late final List<Urls> urls;
   late final int startYear;
@@ -125,13 +33,11 @@ class Results {
   late final Stories stories;
   late final Comics comics;
   late final Events events;
-  late final Next? next;
-  late final void previous;
 
-  Results.fromJson(Map<String, dynamic> json) {
+  SeriesModel.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     title = json['title'];
-    description = null;
+    description = json['description'] ?? 'Description not given :(';
     resourceURI = json['resourceURI'];
     urls = List.from(json['urls']).map((e) => Urls.fromJson(e)).toList();
     startYear = json['startYear'];
@@ -145,8 +51,6 @@ class Results {
     stories = Stories.fromJson(json['stories']);
     comics = Comics.fromJson(json['comics']);
     events = Events.fromJson(json['events']);
-    next = null;
-    previous = null;
   }
 
   Map<String, dynamic> toJson() {
@@ -167,7 +71,6 @@ class Results {
     _data['stories'] = stories.toJson();
     _data['comics'] = comics.toJson();
     _data['events'] = events.toJson();
-    _data['next'] = next;
     return _data;
   }
 }
@@ -380,27 +283,6 @@ class Events {
     _data['collectionURI'] = collectionURI;
     _data['items'] = items;
     _data['returned'] = returned;
-    return _data;
-  }
-}
-
-class Next {
-  Next({
-    required this.resourceURI,
-    required this.name,
-  });
-  late final String resourceURI;
-  late final String name;
-
-  Next.fromJson(Map<String, dynamic> json) {
-    resourceURI = json['resourceURI'];
-    name = json['name'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final _data = <String, dynamic>{};
-    _data['resourceURI'] = resourceURI;
-    _data['name'] = name;
     return _data;
   }
 }
